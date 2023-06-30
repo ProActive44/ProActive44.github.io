081a0533283c870923555383c0984950ddab7e59
Prasad <prasadmhaske2001@gmail.com>
2023-06-30 16:06:30 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <Box className="App" maxW={"1366px"} m={"auto"}>
+    <Box className="App" maxW={"1366px"} m={"auto"} overflow={'hidden'}>
       <Navbar />
       <Home />
       <About />
